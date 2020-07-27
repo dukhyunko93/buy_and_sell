881c352d8e384bded18c821610cdec57416fd838
dukhyunko93 <dukhyunko93@gmail.com>
2020-07-27 16:55:32 -0400

    fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-    belongs_to :user, optional: true
+    belongs_to :seller, :class_name => :User, eoptional: true
     belongs_to :purchased_item , optional: true
     has_many :reviews
     has_many :pending_cart
