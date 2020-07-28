3b2e1a88da6383890c185785cc1b816ae70db715
dukhyunko93 <dukhyunko93@gmail.com>
2020-07-28 16:37:11 -0400

    4:37

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,20 @@
 class SessionsController < ApplicationController
 
-  def new
-      @user = User.new
-  end
+    def new
+        @user = User.new
+    end
   
-  def create
-      user = User.find_by(user_name: params[:user][:user_name])
-      user = user.try(:authenticate, params[:user][:password])
-      return redirect_to(controller: 'sessions', action: 'new') unless user
-    
-      session[:user_id] = user.id
-      redirect_to controller: 'welcome', action: 'home'
-  end
+    def create
+        user = User.find_by(user_name: params[:user][:user_name])
+
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id 
+            redirect_to root_path
+        else
+            flash[:error] = "Username or Password is incorrect"
+            redirect_to login_path
+        end
+    end
   
   def destroy
       session.delete :user_id
