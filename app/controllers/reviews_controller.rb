0f502a7f73ec31b70a073226b859d0f20e2765ac
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-07-29 11:24:59 -0400

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,21 @@
 class ReviewsController < ApplicationController
 
   def new
-
+    @purchased_item = PurchasedItem.find(params[:id])
+    @review = Review.new
   end
-  
+
+  def create
+    review = Review.create(review_params)
+    # product = Product.find(params[:id])
+
+    redirect_to root_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating, :product_id, :purchased_item_id)
+  end
+
 end
