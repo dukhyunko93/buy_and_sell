7587e521a3984a2b23cc1a17bb55a7660462d39c
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-07-30 13:11:30 -0400

    average

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,16 @@ class User < ApplicationRecord
     validates_presence_of :name, :user_name, :address, :email, :phone_number, :dob
     validates_uniqueness_of :name
 
-    
+
     def seller_reviews
         Review.all.select {|t| t.seller_id == self.id}
     end
+
+    def avg
+      self.seller_reviews.map do |r|
+        r.rating.inject{ |sum, el| sum + el }.to_f / r.size
+      end
+    end
+
 
 end
