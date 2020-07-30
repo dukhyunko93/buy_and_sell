b64e73595a733aa43050b2f656dbdc29378251d6
dukhyunko93 <dukhyunko93@gmail.com>
2020-07-30 13:08:38 -0400

    lunchbreak

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
     end
 
     def edit
+        
     end
 
     def update
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,7 @@ class Review < ApplicationRecord
     belongs_to :buyer, :class_name => :User
     belongs_to :seller, :class_name => :User
 
+
+    #Creat avg_rating
+
 end
