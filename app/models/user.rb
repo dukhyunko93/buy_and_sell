8530a6fd58cf68795eeeaf495248d575e393cd20
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-07-30 00:13:21 -0400

    css

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
     has_many :pending_carts, :foreign_key => 'buyer_id'
     has_many :purchased_items, :foreign_key => 'buyer_id'
     has_many :reviews
+    validates :name, presence: true
+    validates :name, uniqueness: true
+
+    validates :user_name, presence: true
+    validates :user_name, uniqueness: true
 
     def seller_reviews
         Review.all.select {|t| t.seller_id == self.id}
